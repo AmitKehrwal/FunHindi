f5ba5df1f10ae30613999fe839e0b44202473643
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-10-12 05:19:03 +0530

    Update arun.py

--- arun.py
+++ arun.py
@@ -3,9 +3,7 @@ import threading
 from concurrent.futures import ThreadPoolExecutor
 from playwright.async_api import async_playwright
 import nest_asyncio
-import random
-import argparse
-import getindianname as name
+from faker import Faker  # Import the Faker library for generating Spanish names
 
 nest_asyncio.apply()
 
@@ -58,6 +56,38 @@ async def start(name, user, wait_time, meetingcode, passcode):
 
         await browser.close()
 
+async def main():
+    global running
+    number = int(input("Enter number of Users: "))
+    meetingcode = input("Enter meeting code (No Space): ")
+    passcode = input("Enter Password (No Space): ")
 
+    sec = 90
+    wait_time = sec * 60
 
+    # Create a Faker instance with the 'es_ES' locale for generating Spanish names
+    fake = Faker('es_ES')
 
+    with ThreadPoolExecutor(max_workers=number) as executor:
+        loop = asyncio.get_running_loop()
+        tasks = []
+        for i in range(number):
+            try:
+                # Generate a random Spanish name using Faker
+                user = fake.name()
+            except IndexError:
+                break
+            task = loop.create_task(start(f'[Thread{i}]', user, wait_time, meetingcode, passcode))
+            tasks.append(task)
+        try:
+            await asyncio.gather(*tasks)
+        except KeyboardInterrupt:
+            running = False
+            # Wait for tasks to complete
+            await asyncio.gather(*tasks, return_exceptions=True)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
